48e99c224c938a1cd13de350580cc14472608ccd
kuroudo119 <43639887+kuroudo119@users.noreply.github.com>
2018-10-31 14:01:41 +0900

    顔画像の表示も選択できるようにした。

--- KRD_EquipActor.js
+++ KRD_EquipActor.js
@@ -20,6 +20,11 @@
  * Default: 50
  * @default 50
  * 
+ * @param UseFace
+ * @desc Input true to use face image. Please change X=150, Y=0
+ * Default: false
+ * @default false
+ * 
  */
 
 (function() {
@@ -29,6 +34,7 @@
 var parameters = PluginManager.parameters('KRD_EquipActor');
 var x = Number(parameters['PositionX']);
 var y = Number(parameters['PositionY']);
+var useFace = parameters['UseFace'] === 'true';
 
 //================================================
 
@@ -36,7 +42,11 @@ var KRD_Window_EquipStatus_refresh = Window_EquipStatus.prototype.refresh;
 Window_EquipStatus.prototype.refresh = function() {
     KRD_Window_EquipStatus_refresh.call(this);
     if (this._actor) {
-        this.drawActorCharacter(this._actor, x, y);
+        if (useFace) {
+            this.drawActorFace(this._actor, x, y, 144, 36);
+        } else {
+            this.drawActorCharacter(this._actor, x, y);
+        }
     }
 };
 
