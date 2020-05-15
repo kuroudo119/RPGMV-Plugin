9462b6a1d1dccdd82ed5c7e5216b121add42c40a
kuroudo119 <43639887+kuroudo119@users.noreply.github.com>
2020-05-15 19:21:25 +0900

    リフレッシュ処理を減らすのを、ターン終了時処理のみに変更した。

--- KRD_Refresh.js
+++ KRD_Refresh.js
@@ -35,54 +35,5 @@ BattleManager.endTurn = function() {
     }
 };
 
-// BattleManager.processTurn = function() {
-//     var subject = this._subject;
-//     var action = subject.currentAction();
-//     if (action) {
-//         action.prepare();
-//         if (action.isValid()) {
-//             this.startAction();
-//         }
-//         subject.removeCurrentAction();
-//     } else {
-//         subject.onAllActionsEnd();
-//         this.refreshStatus();
-//         this._logWindow.displayAutoAffectedStatus(subject);
-//         this._logWindow.displayCurrentState(subject);
-//         this._logWindow.displayRegeneration(subject);
-//         this._subject = this.getNextSubject();
-//     }
-// };
-
-BattleManager.startAction = function() {
-    var subject = this._subject;
-    var action = subject.currentAction();
-    var targets = action.makeTargets();
-    this._phase = 'action';
-    this._action = action;
-    this._targets = targets;
-    subject.useItem(action.item());
-    this._action.applyGlobal();
-    // this.refreshStatus();
-    this._logWindow.startAction(subject, action, targets);
-};
-
-BattleManager.invokeAction = function(subject, target) {
-    this._logWindow.push('pushBaseLine');
-    if (Math.random() < this._action.itemCnt(target)) {
-        this.invokeCounterAttack(subject, target);
-    } else if (Math.random() < this._action.itemMrf(target)) {
-        this.invokeMagicReflection(subject, target);
-    } else {
-        this.invokeNormalAction(subject, target);
-    }
-    subject.setLastTarget(target);
-    this._logWindow.push('popBaseLine');
-    // this.refreshStatus();
-    if (subject.isActor() || target.isActor()) {
-        this.refreshStatus();
-    }
-};
-
 //================================================
 })();
